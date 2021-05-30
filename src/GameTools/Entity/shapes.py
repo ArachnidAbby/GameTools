2de0039550fe7fa4a3feb06477f04e00a15ccdaa
spidertyler2005 <spidertyler1122@gmail.com>
2021-05-29 20:25:22 -0400

    made "cached_info" global to improve render times

--- src/GameTools/Entity/shapes.py
+++ src/GameTools/Entity/shapes.py
@@ -2,25 +2,29 @@ from . import basics
 import GameTools.Tools as Tools
 import pygame
 
+global cached_info
+cached_info = [-1,-1,-1]
+
 class rectangle(basics.Entity):
     def __init__(self, plane,color):
         super().__init__(plane)
         self.color = color
-        self.cached_info=[100,100,0]
+        #self.cached_info=[100,100,0]
 
     def draw(self,game):
-        scaleFactor=self.cached_info
-        if [game.currentWidth,game.currentHeight] != self.cached_info[0::1]:
+        global cached_info
+        scaleFactor=cached_info
+        if [game.currentWidth,game.currentHeight] != cached_info[0::1]:
             scaleFactor = [
                 game.currentWidth,
                 game.currentHeight,
                 ((game.currentWidth**2)+(game.currentHeight**2))**0.5
             ]
-            self.cached_info = scaleFactor
+            cached_info = scaleFactor
         plane = self.plane.export()
         plane = [
-            plane[0]*scaleFactor[2],
-            plane[1]*scaleFactor[2],
+            plane[0]*scaleFactor[0],
+            plane[1]*scaleFactor[1],
             plane[2]*scaleFactor[2],
             plane[3]*scaleFactor[2],
         ]
